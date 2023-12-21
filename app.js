79ca8a7ddf0b05080fa5a9fbfb37ce910b3600fc
Dark Space <150314290+darkspace123098@users.noreply.github.com>
2023-12-21 16:19:00 +0000

    object

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
-// console.log(Array.from("Rockstar"));
+// // console.log(Array.from("Rockstar"));
 
-const mysym = Symbol("key1")
+// const mysym = Symbol("key1")
 
-const obj1 = {
-    [mysym] : "My infor"
+// const obj1 = {
+//     [mysym] : "My infor"
+// }
+// console.log(obj1[mysym]);
+
+// function cart(...num1) {
+//     return num1
+// }
+// console.log(cart(450,200,900,700));
+
+const course = {
+    coursename : "React-js",
+    price : "2399",
+    courseInstructor : "Brayan"
+
 }
-console.log(obj1[mysym]);
+
+const {courseInstructor : teacher, price : RS} = course
+console.log(teacher);
+console.log(RS);
