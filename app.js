93c735f0a52af035dedc38375dc99165e6ecf9ec
Dark Space <150314290+darkspace123098@users.noreply.github.com>
2023-12-21 16:31:33 +0000

    arrow function

--- app.js
+++ app.js
@@ -12,13 +12,28 @@
 // }
 // console.log(cart(450,200,900,700));
 
-const course = {
-    coursename : "React-js",
-    price : "2399",
-    courseInstructor : "Brayan"
+// const course = {
+//     coursename : "React-js",
+//     price : "2399",
+//     courseInstructor : "Brayan"
 
-}
+// }
 
-const {courseInstructor : teacher, price : RS} = course
-console.log(teacher);
-console.log(RS);
+// const {courseInstructor : teacher, price : RS} = course
+// console.log(teacher);
+// console.log(RS);
+
+// const user = {
+//     username : "Shayan",
+//     price : 1289,
+
+//     welcomeMessage : function(){
+//         console.log(`${this.username}, Welcome To Codespace`);
+//     }
+
+// }
+// user.username = "sunjay"
+// user.welcomeMessage()
+
+// const addTwo = (num1,num2) => (num1+num2)
+// console.log(addTwo(3,6));
