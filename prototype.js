787222bc9aee5710d6edbe2402798d6609ea4f9e
Dark Space <150314290+darkspace123098@users.noreply.github.com>
2024-05-04 06:04:53 +0000

    com

--- prototype.js
+++ prototype.js
@@ -8,12 +8,24 @@ createUser.prototype.increement = function () {
     this.price++
 }
 createUser.prototype.print = function(){
+    console.log(this.username)
     console.log("price is :"+this.price);
 }
+createUser.prototype.upper = function (){
+    console.log(this.username.toUpperCase()); 
+}
+createUser.prototype.trueLen = function (){
+    console.log(this.username.trim(" ").length);
+}
 
-const chai = new createUser("light chai",30)
+const chai = new createUser("light chai   ",30)
+const poori = new createUser("Poori bhaji",70)
 
-// chai.increement()
-// chai.increement()
 
+chai.increement()
 chai.print()
+chai.upper()
+chai.trueLen()
+
+poori.increement()
+poori.print()
