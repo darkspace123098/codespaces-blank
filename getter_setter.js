b68d13eac140cae205a26eba5c725001631a185f
Dark Space <150314290+darkspace123098@users.noreply.github.com>
2024-06-07 06:39:37 +0000

    get_set

---
+++ getter_setter.js
@@ -0,0 +1,14 @@
+class User {
+
+    constructor(email,password){
+        this.email = email;
+        this.password = password;
+    }
+
+    get password(){
+        return ``
+    }
+    set password(value){
+        
+    }
+}
