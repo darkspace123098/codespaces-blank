34ce659a40d67b2bd74a4cfa469bf16f0da44ac8
Dark Space <150314290+darkspace123098@users.noreply.github.com>
2024-06-07 11:35:36 +0000

    getter_pro.js

--- getter_setter.js
+++ getter_setter.js
@@ -6,18 +6,24 @@ class User {
     }
 
     get password(){
-        return `user${this._password}firecode`
+        return `ffff${this._password}ffff`
     }
     set password(value){
         this._password = value;
     }
 }
+
+
 function getpass(){
     const pass = document.getElementById("code").value;
 const admin = new User("admin342@gmail.com",pass)
 if(pass != "")
-document.getElementById("info").innerText = admin.password
+    {
+        document.getElementById("info").innerText = admin.password
+        console.log(admin.password)
+    }
 else
   alert("Input requred!!")
    
 }
+
