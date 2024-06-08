b1a9b7dc60527944d260921978e4ad0dfe3bd539
Dark Space <150314290+darkspace123098@users.noreply.github.com>
2024-06-08 10:23:19 +0000

    array

---
+++ JSFiles/array.js
@@ -0,0 +1,24 @@
+const myArr = [2,7,"first",10,true]
+// console.log(myArr);
+
+// myArr.push(5,"pushed text")
+// console.log(myArr);
+
+// myArr.pop()
+// myArr.pop()
+// myArr.pop()
+// myArr.shift()
+// myArr.unshift(9)
+// console.log(myArr);
+// console.log(myArr.length); 
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf("first"));
+// console.log(typeof myArr);
+
+// const newArr = myArr.join();
+// console.log(newArr);
+// console.log(typeof newArr);
+
+const sArray = myArr.slice(1,5)
+console.log(sArray);
